7e4734df59ca92e22c12d61cccad2ec031d0affe
TuningLemon <veststar1@gmail.com>
2020-07-19 21:58:50 +0900

    modifying import variable ffrom aws-exports

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import Amplify from "aws-amplify";
-import config from "./aws-exports";
+import awsmobile from "./aws-exports";
 
-Amplify.configure(config);
+Amplify.configure(awsmobile);
 ReactDOM.render(
   <React.StrictMode>
     <App />
